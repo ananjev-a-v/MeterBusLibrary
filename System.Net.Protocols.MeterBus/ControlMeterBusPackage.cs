dffd417ab940ef479169f380c8f6c15ffb75db5c
Martin Dahlstedt <md@eniga.io>
2018-07-14 16:47:05 +0200

    Added control frame serialization.

--- System.Net.Protocols.MeterBus/ControlMeterBusPackage.cs
+++ System.Net.Protocols.MeterBus/ControlMeterBusPackage.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace System.Net.Protocols.MeterBus
 {
     public sealed class ControlMeterBusPackage : MeterBusPackage
     {
-        public ControlMeterBusPackage()
-        {
+        private readonly ControlMask _control;
+        private readonly ControlInformationMask _controlInformation;
+        private readonly byte _address;
+        private readonly byte _crc;
 
+        public ControlMeterBusPackage(ControlMask control, ControlInformationMask controlInformation, byte address)
+        {
+            _control = control;
+            _controlInformation = controlInformation;
+            _address = address;
+            var data = new byte[] { (byte)_control, _address, (byte)_controlInformation };
+            _crc = CheckSum(data, 0, data.Length);
+        }
+
+        internal void Write(Stream stream)
+        {
+            stream.WriteByte((byte)ResponseCodes.CONTROL_FRAME_START);
+            stream.WriteByte((byte)3);
+            stream.WriteByte((byte)3);
+            stream.WriteByte((byte)ResponseCodes.CONTROL_FRAME_START);
+            stream.WriteByte(_crc);
+            stream.WriteByte((byte)ResponseCodes.FRAME_END);
         }
     }
 }
