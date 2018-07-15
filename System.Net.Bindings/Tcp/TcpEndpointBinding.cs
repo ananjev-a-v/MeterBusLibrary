7df7160d120e10b15fea52c7507ec4b66faa2194
Martin Dahlstedt <md@eniga.io>
2018-07-15 18:07:51 +0200

    First version of tcp binding.

--- System.Net.Bindings/Tcp/TcpEndpointBinding.cs
+++ System.Net.Bindings/Tcp/TcpEndpointBinding.cs
@@ -5,12 +5,12 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace System.Net.Bindings.Udp
+namespace System.Net.Bindings.Tcp
 {
-    public sealed class UdpEndpointBinding : EndpointBinding, IEndPointBinding, IDisposable
+    public sealed class TcpEndpointBinding : EndpointBinding, IEndPointBinding, IDisposable
     {
         private const int MAX_BUFFER_SIZE = 2048;
-        private readonly Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private readonly Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         private readonly EndPoint _endpoint;
 
         /// <summary>
@@ -21,9 +21,21 @@ namespace System.Net.Bindings.Udp
         /// Server: new IPEndPoint(IPAddress.Any, 1690)</param>
         /// <param name="serializer"></param>
         /// <param name="logger"></param>
-        public UdpEndpointBinding(EndPoint endpoint, IPacketSerializer serializer, ITelemetryLogger logger) : base(serializer, logger)
+        public TcpEndpointBinding(EndPoint endpoint, IPacketSerializer serializer, ITelemetryLogger logger) : base(serializer, logger)
         {
             _endpoint = endpoint ?? throw new ArgumentNullException(nameof(endpoint));
+        }
+
+        public bool Connect()
+        {
+            var args = new SocketAsyncEventArgs();
+            args.RemoteEndPoint = _endpoint;
+            args.AcceptSocket = _socket;
+            args.UserToken = _socket;
+            args.SetBuffer(new byte[MAX_BUFFER_SIZE], 0, MAX_BUFFER_SIZE);
+            args.Completed += OnClientOperation;
+            
+            return _socket.ConnectAsync(args);
         }
 
         public bool Send(INetworkPacket package)
@@ -43,10 +55,11 @@ namespace System.Net.Bindings.Udp
             args.RemoteEndPoint = _endpoint;
             args.SetBuffer(data, 0, length);
             args.AcceptSocket = _socket;
+            args.UserToken = _socket;
             args.UserToken = package;
             args.Completed += OnClientOperation;
 
-            return _socket.SendToAsync(args);
+            return _socket.SendAsync(args);
         }
 
         public bool StartReceiving()
@@ -56,6 +69,7 @@ namespace System.Net.Bindings.Udp
             var args = new SocketAsyncEventArgs();
             args.RemoteEndPoint = _endpoint;
             args.AcceptSocket = _socket;
+            args.UserToken = _socket;
             args.SetBuffer(new byte[MAX_BUFFER_SIZE], 0, MAX_BUFFER_SIZE);
             args.Completed += OnServerOperation;
 
@@ -73,7 +87,7 @@ namespace System.Net.Bindings.Udp
                     case SocketError.Success:
                         {
                             switch (e.LastOperation)
-                            {
+                            {                            
                                 case SocketAsyncOperation.SendTo:
                                     {
 
@@ -81,48 +95,7 @@ namespace System.Net.Bindings.Udp
                                     break;
                                 case SocketAsyncOperation.ReceiveFrom:
                                     {
-                                        _packageStream.OnNext(new PacketStreamEvent(_serializer.Deserialize(e.Buffer), new UdpEndpointBinding(e.RemoteEndPoint, _serializer, _logger)));
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-                _packageStream.OnError(ex);
-            }
-            finally
-            {
-                var receiveBuffer = new byte[MAX_BUFFER_SIZE];
-                e.SetBuffer(receiveBuffer, 0, receiveBuffer.Length);
-                var result = e.AcceptSocket.ReceiveFromAsync(e);
-            }
-        }
-
-        private void OnClientOperation(object sender, SocketAsyncEventArgs e)
-        {
-            try
-            {
-                switch (e.SocketError)
-                {
-                    case SocketError.Success:
-                        {
-                            switch (e.LastOperation)
-                            {
-                                case SocketAsyncOperation.SendTo:
-                                    {
-
-                                    }
-                                    break;
-                                case SocketAsyncOperation.Receive:
-                                    {
-                                        _packageStream.OnNext(new PacketStreamEvent(_serializer.Deserialize(e.Buffer), new UdpEndpointBinding(e.RemoteEndPoint, _serializer, _logger)));
+                                        _packageStream.OnNext(new PacketStreamEvent(_serializer.Deserialize(e.Buffer), new TcpEndpointBinding(e.RemoteEndPoint, _serializer, _logger)));
                                     }
                                     break;
                                 default:
@@ -144,7 +117,53 @@ namespace System.Net.Bindings.Udp
                 e.SetBuffer(receiveBuffer, 0, receiveBuffer.Length);
                 var result = e.AcceptSocket.ReceiveAsync(e);
             }
-        }      
+        }
+
+        private void OnClientOperation(object sender, SocketAsyncEventArgs e)
+        {
+            try
+            {
+                switch (e.SocketError)
+                {
+                    case SocketError.Success:
+                        {
+                            switch (e.LastOperation)
+                            {
+                                case SocketAsyncOperation.Connect:
+                                    {
+
+                                    }
+                                    break;
+                                case SocketAsyncOperation.Send:
+                                    {
+
+                                    }
+                                    break;
+                                case SocketAsyncOperation.Receive:
+                                    {
+                                        _packageStream.OnNext(new PacketStreamEvent(_serializer.Deserialize(e.Buffer), new TcpEndpointBinding(e.RemoteEndPoint, _serializer, _logger)));
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                _packageStream.OnError(ex);
+            }
+            finally
+            {
+                var receiveBuffer = new byte[MAX_BUFFER_SIZE];
+                e.SetBuffer(receiveBuffer, 0, receiveBuffer.Length);
+                var result = e.AcceptSocket.ReceiveAsync(e);
+            }
+        }       
 
         public void Dispose()
         {
@@ -155,7 +174,7 @@ namespace System.Net.Bindings.Udp
         {
             switch (_endpoint)
             {
-                case IPEndPoint ipEndpoint: return $"Udp {ipEndpoint.Address.ToString()}:{ipEndpoint.Port}";
+                case IPEndPoint ipEndpoint: return $"Tcp {ipEndpoint.Address.ToString()}:{ipEndpoint.Port}";
                 default: return base.ToString();
             }
         }
